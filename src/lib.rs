7147d460452e9fa6964eacf828677b7eca95db79
Patrick Bucher <patrick.bucher@mailbox.org>
2024-04-05 08:41:31 +0200

    prompt function

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,30 @@
+use std::io;
+use std::io::Error;
+use std::io::Write;
+use std::result::Result;
+use std::str::FromStr;
+
 pub fn round_to(value: f64, granularity: f64) -> f64 {
     let scale_factor = 1.0 / granularity;
     let scaled_up = value * scale_factor;
     let rounded = scaled_up.round();
     let scaled_down = rounded / scale_factor;
     scaled_down
+}
+
+pub fn prompt_line<T: FromStr<Err = std::io::Error>>(prompt: &str) -> Result<T, Error> {
+    print!("{prompt}");
+    io::stdout().flush()?;
+    let mut input = String::new();
+    io::stdin().read_line(&mut input)?;
+
+    // FIXME: why does this fail?
+    // input.parse()?
+
+    match input.parse() {
+        Ok(val) => Ok(val),
+        Err(err) => Err(err),
+    }
 }
 
 #[cfg(test)]
