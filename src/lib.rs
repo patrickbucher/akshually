d6751484822464385db1cb483485abf0af372a3b
Patrick Bucher <patrick.bucher@mailbox.org>
2024-04-05 08:09:30 +0200

    provide round_to function

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,9 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+pub fn round_to(value: f64, granularity: f64) -> f64 {
+    let scale_factor = 1.0 / granularity;
+    let scaled_up = value * scale_factor;
+    let rounded = scaled_up.round();
+    let scaled_down = rounded / scale_factor;
+    scaled_down
 }
 
 #[cfg(test)]
@@ -7,8 +11,8 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn round_to_nickel() {
+        let result = round_to(10.0 / 3.0, 0.05);
+        assert_eq!(result, 3.35);
     }
 }
