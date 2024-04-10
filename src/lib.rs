2cffaf0192ea7667110ce82eb935926a007fc545
Patrick Bucher <patrick.bucher@mailbox.org>
2024-04-10 07:48:10 +0200

    doc comments, license

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,19 @@
+//! # Akshually
+//!
+//! The `akshually` crate contains various unrelated functions I kept on re-implementing for
+//! different projects and examples. Therefore I started collecting them in a library that doesn't
+//! take up any additional dependencies.
+
 use std::io;
 use std::io::Write;
 
+/// Rounds the `value` with the specified `granularity`.
+///
+/// # Examples
+///
+/// ```
+/// assert_eq!(akshually::round_to(1.93333, 0.05), 1.95);
+/// ```
 pub fn round_to(value: f64, granularity: f64) -> f64 {
     let scale_factor = 1.0 / granularity;
     let scaled_up = value * scale_factor;
@@ -9,6 +22,21 @@ pub fn round_to(value: f64, granularity: f64) -> f64 {
     scaled_down
 }
 
+/// Prompts the user (without a newline) to input a value.
+///
+/// # Examples
+///
+/// ```
+/// let input = akshually::prompt_line::<u8>("Your age: ");
+/// match input {
+///     Some(age) => {
+///         println!("You are {age} years old.");
+///     },
+///     None => {
+///         println!("You didn't enter an age.");
+///     },
+/// };
+/// ```
 pub fn prompt_line<T: std::str::FromStr>(prompt: &str) -> Option<T> {
     print!("{prompt}");
     if let Ok(_) = io::stdout().flush() {
